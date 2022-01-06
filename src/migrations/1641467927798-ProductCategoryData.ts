c27b75b3ea9cb9350d8779d24354df521a0478eb
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-06 18:13:59 +0100

    feat: add all product categories

--- src/migrations/1641467927798-ProductCategoryData.ts
+++ src/migrations/1641467927798-ProductCategoryData.ts
@@ -1,38 +1,17 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import {
+  accessoriesCategories,
+  clothesCategories,
+  lifestyleCategories,
+  shoesCategories,
+} from '../migration-utils/category.data';
 import { ProductCategory } from '../modules/product-category/entities/product-category.entity';
 export class ProductCategoryData1641467927798 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const clothes = {
-      name: 'Clothes',
-      slug: '/clothes',
-      path: '/clothes',
-      description:
-        "Among the more than 250 brands we offer, you will find giants such as GANT, Morris, Calvin Klein, Peak Performance and many, many more. Our men's clothes are carefully selected to follow the latest trends and at the same time be both high quality and affordable. A good wardrobe, just like a web shop, needs to have both width and depth to be ready for everything. Nothing consciously says anything as well as a perfect shirt or pair of really nice jeans bras. Choose and wreck among pants, shorts, jackets, suits, sweaters, underwear and everything else that a modern man's wardrobe needs to be equipped with. Order your clothes for men from Stayhard - we help you take your style to a whole new level",
-      children: [
-        {
-          name: 'Sweaters',
-          slug: '/sweaters',
-          path: '/clothes/sweaters',
-          description:
-            "Sweaters say a lot about a man's style as it is a garment that has the potential to both capture attention and give the wearer a neutral style. Some shirts are used to spread messages with prints and texts, others are solid color and without any major details. Your style and taste determine what is right for you, but a rule of thumb is that the more stylish the model is, the easier it is to match. Solid-colored sweaters in simple cuts are therefore also best suited for work and for more formal occasions, even if they also work at home. In your free time and at the gym, you just have to go all out with colors and prints - here you set the limits yourself. For spring and summer, cool, light sweaters are needed that keep you warm on cold evenings. For winter and autumn, you should invest in warm, knitted models - preferably with a slightly looser fit so that you can hold a few layers underneath.",
-        },
-        {
-          name: 'T-shirts & polo shirts',
-          description: 'any',
-          path: '/clothes/t-shirts-polo-shirts',
-          slug: '/t-shirts-polo-shirts',
-          children: [
-            {
-              name: 'Solid color T-Shirts',
-              description: 'ok mate',
-              path: '/clothes/t-shirts-polo-shirts/solid-color-t-shirts',
-              slug: '/solid-color-t-shirts',
-            },
-          ],
-        },
-      ],
-    };
-    await ProductCategory.create(clothes).save();
+    await ProductCategory.create(clothesCategories).save();
+    await ProductCategory.create(lifestyleCategories).save();
+    await ProductCategory.create(shoesCategories).save();
+    await ProductCategory.create(accessoriesCategories).save();
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
