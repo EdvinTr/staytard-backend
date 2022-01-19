cfdf3cf969816cecfdd58391d52a70bd7dcb6713
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-19 14:03:23 +0100

    test: find all products

--- src/modules/product/tests/product.service.spec.ts
+++ src/modules/product/tests/product.service.spec.ts
@@ -5,7 +5,9 @@ import { ProductService } from '../product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
-  const mockProductRepository = {};
+  const mockProductRepository = {
+    findAndCount: jest.fn().mockReturnValue([[], 0]),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -19,9 +21,29 @@ describe('ProductService', () => {
 
     service = module.get<ProductService>(ProductService);
   });
-
-  it('should be defined', () => {
-    expect(1).toBe(1);
+  describe('when querying all products', () => {
+    const totalProductCount = 10;
+    beforeEach(() => {
+      mockProductRepository.findAndCount = jest
+        .fn()
+        .mockReturnValue([[], totalProductCount]);
+    });
+    it('should indicate there are more products if limit is less than total product count', async () => {
+      const { hasMore, totalCount } = await service.findAll({
+        categoryPath: '/clothes',
+        limit: 5,
+        offset: 0,
+      });
+      expect(hasMore).toBeTruthy();
+      expect(totalCount).toEqual(totalProductCount);
+    });
+    it('should indicate there are no more products if limit is equal to total product count', async () => {
+      const { hasMore } = await service.findAll({
+        categoryPath: '/clothes',
+        limit: totalProductCount,
+        offset: 0,
+      });
+      expect(hasMore).toBeFalsy();
+    });
   });
-  // TODO: test that hasMore returns correct value depending on the offset & limit & count variable calculation
 });
