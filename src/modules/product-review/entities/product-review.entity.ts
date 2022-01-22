df3c64e9d15ebc11459fbffa0f1105dfe6ab22da
Ehno1999 <Rasmus.johansson599@gmail.com>
2022-01-22 15:57:08 +0100

    entity

--- src/modules/product-review/entities/product-review.entity.ts
+++ src/modules/product-review/entities/product-review.entity.ts
@@ -1,5 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+import {time, timezone, unix_time} from "casual";
 
 // TODO:
 // 1. Should reference products, (FK should be in this ProductReview entity)
@@ -10,4 +11,39 @@ export class ProductReview {
   @Field()
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Field()
+  title:Text;
+
+  @Field()
+  content:Text;
+
+  @Field()
+  would_recommend:boolean;
+
+  @Field()
+  rating:number;
+
+  @Field()
+  submitted_by_alias:Text;
+
+  @Field()
+  email:Text;
+
+  @Field()
+  is_published:boolean;
+
+
+  @Column("timestamp")
+  published:Text;
+
+
+  @Column("timestamp")
+  created_at:Text;
+
+
+  @Column("timestamp")
+  update_at:Text;
+
+
 }
