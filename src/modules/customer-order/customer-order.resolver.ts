34eb472ce4996d6f0a8cfb1431fe0ef9623e71f8
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-02-03 10:17:17 +0100

    refactor: remove unused code

--- src/modules/customer-order/customer-order.resolver.ts
+++ src/modules/customer-order/customer-order.resolver.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from '@nestjs/common';
-import { Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Context, Query, Resolver } from '@nestjs/graphql';
 import { GraphqlJwtAuthGuard } from '../authentication/guards/graphql-jwt-auth.guard';
 import RequestWithUser from '../authentication/interfaces/request-with-user.interface';
 import { CustomerOrderService } from './customer-order.service';
@@ -15,25 +15,5 @@ export class CustomerOrderResolver {
     @Context() { req }: { req: RequestWithUser },
   ): Promise<CustomerOrder[]> {
     return this.customerOrderService.findAll(req.user.id);
-  }
-
-  @Mutation(() => String)
-  async testCreateCustomerOrder() {
-    return this.customerOrderService.create(
-      {
-        city: 'test',
-        deliveryAddress: 'test',
-        orderItems: [
-          { productId: 823, sku: 'PAPN-WHI-L4', quantity: 1 },
-          { productId: 487, sku: 'JSHJ-WHI-S5', quantity: 1 },
-        ],
-        orderNumber: 'test',
-        paymentType: 'card',
-        postalCode: '75645',
-        purchaseCurrency: 'SEK',
-        totalAmount: 100,
-      },
-      'userId',
-    );
   }
 }
