60d00f0068f5f0236a6aa28d1a9bc7843331d78b
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-02-14 13:18:49 +0100

    add: basic categories endpoint

--- src/modules/product-category/product-category.resolver.ts
+++ src/modules/product-category/product-category.resolver.ts
@@ -1,4 +1,6 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
+import { IsNull, Not } from 'typeorm';
+import { BasicCategory } from './dto/basic-category';
 import { ProductCategory } from './entities/product-category.entity';
 import { ProductCategoryService } from './product-category.service';
 
@@ -15,6 +17,24 @@ export class ProductCategoryResolver {
       where: {
         parent: null,
       },
+    });
+  }
+
+  @Query(() => [BasicCategory])
+  async basicCategories(): Promise<BasicCategory[]> {
+    return this.productCategoryService.findCategories({
+      relations: [
+        'children',
+        'children.children',
+        'children.children.children',
+      ],
+      where: {
+        parent: Not(IsNull()),
+      },
+      order: {
+        name: 'ASC',
+      },
+      select: ['id', 'name'],
     });
   }
 
