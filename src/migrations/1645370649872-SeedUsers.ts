c0a9a216fa09c59dccc3552199f9429754a7812b
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-02-20 17:51:47 +0100

    seed: users

---
+++ src/migrations/1645370649872-SeedUsers.ts
@@ -0,0 +1,33 @@
+import * as bcrypt from 'bcrypt';
+import * as casual from 'casual';
+import { getRepository, MigrationInterface, QueryRunner } from 'typeorm';
+import { User } from '../modules/user/entities/user.entity';
+
+export class SeedUsers1645370649872 implements MigrationInterface {
+  public async up(): Promise<void> {
+    const userRepo = getRepository(User);
+    for (let i = 0; i < 5000; i++) {
+      const user = userRepo.create({
+        firstName: casual.first_name,
+        lastName: casual.last_name,
+        email: casual.email,
+        address: {
+          street: casual.street,
+          city: casual.city,
+          postalCode: casual.zip(5),
+        },
+        isEmailConfirmed: false,
+        isRegisteredWithGoogle: false,
+        password: await bcrypt.hash('123123123a', 10),
+        mobilePhoneNumber: casual.phone,
+        permissions: [],
+      });
+      userRepo.save(user);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`TRUNCATE public.user CASCADE;`);
+    await queryRunner.query(`DELETE FROM user_address;`);
+  }
+}
