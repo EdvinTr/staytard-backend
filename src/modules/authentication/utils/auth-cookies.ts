be67bb64355b76d703abd34e0ae45745b49c5a6b
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-03-06 15:49:49 +0100

    test: domain

--- src/modules/authentication/utils/auth-cookies.ts
+++ src/modules/authentication/utils/auth-cookies.ts
@@ -10,11 +10,13 @@ export const setAuthCookies = (
   res.cookie(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME, accessToken, {
     maxAge: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
     sameSite,
+    domain: 'example.com',
     secure,
   });
   res.cookie(process.env.JWT_REFRESH_TOKEN_COOKIE_NAME, refreshToken, {
     maxAge: +process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
     sameSite,
+    domain: 'example.com',
     secure,
   });
 };
@@ -22,10 +24,12 @@ export const setAuthCookies = (
 export const removeAuthCookies = (res: Response) => {
   res.clearCookie(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME, {
     sameSite,
+    domain: 'example.com',
     secure,
   });
   res.clearCookie(process.env.JWT_REFRESH_TOKEN_COOKIE_NAME, {
     sameSite,
+    domain: 'example.com',
     secure,
   });
 };
