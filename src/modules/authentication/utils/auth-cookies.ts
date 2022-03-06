5e0e9d5fd8e3be1ce70f6bd344c27a45af862e06
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-03-06 13:34:37 +0100

    fix: clear cookie

--- src/modules/authentication/utils/auth-cookies.ts
+++ src/modules/authentication/utils/auth-cookies.ts
@@ -1,23 +1,31 @@
 import { Response } from 'express';
 
+const sameSite = process.env.NODE_ENV === 'production' ? 'none' : 'lax';
+const secure = process.env.NODE_ENV === 'production' ? true : false;
 export const setAuthCookies = (
   res: Response,
   accessToken: string,
   refreshToken: string,
 ) => {
   res.cookie(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME, accessToken, {
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
-    secure: process.env.NODE_ENV === 'production' ? true : false,
+    sameSite,
+    secure,
   });
   res.cookie(process.env.JWT_REFRESH_TOKEN_COOKIE_NAME, refreshToken, {
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: +process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
-    secure: process.env.NODE_ENV === 'production' ? true : false,
+    sameSite,
+    secure,
   });
 };
 
 export const removeAuthCookies = (res: Response) => {
-  res.clearCookie(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME);
-  res.clearCookie(process.env.JWT_REFRESH_TOKEN_COOKIE_NAME);
+  res.clearCookie(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME, {
+    sameSite,
+    secure,
+  });
+  res.clearCookie(process.env.JWT_REFRESH_TOKEN_COOKIE_NAME, {
+    sameSite,
+    secure,
+  });
 };
