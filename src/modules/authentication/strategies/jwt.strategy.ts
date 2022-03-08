581833bd52e1d8fb235b56fbe84e1b38a0dbb252
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-03-08 09:57:10 +0100

    fix: use bearer token instead

--- src/modules/authentication/strategies/jwt.strategy.ts
+++ src/modules/authentication/strategies/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from '../../user/entities/user.entity';
 import { UserService } from '../../user/user.service';
@@ -13,13 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private readonly userService: UserService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => {
-          return request?.cookies[
-            configService.get('JWT_ACCESS_TOKEN_COOKIE_NAME')
-          ];
-        },
-      ]),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get('JWT_ACCESS_TOKEN_SECRET'),
     });
