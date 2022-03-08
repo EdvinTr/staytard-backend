013c801bbe37e7b69d714b59ad838394edd65dec
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-03-08 09:28:55 +0100

    set: path

--- src/modules/authentication/utils/auth-cookies.ts
+++ src/modules/authentication/utils/auth-cookies.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 
 const sameSite = process.env.NODE_ENV === 'production' ? 'none' : 'lax';
 const secure = process.env.NODE_ENV === 'production' ? true : false;
+const path = '/';
 export const setAuthCookies = (
   res: Response,
   accessToken: string,
@@ -11,11 +12,13 @@ export const setAuthCookies = (
     maxAge: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
     sameSite,
     secure,
+    path,
   });
   res.cookie(process.env.JWT_REFRESH_TOKEN_COOKIE_NAME, refreshToken, {
     maxAge: +process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
     sameSite,
     secure,
+    path,
   });
 };
 
@@ -23,9 +26,11 @@ export const removeAuthCookies = (res: Response) => {
   res.clearCookie(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME, {
     sameSite,
     secure,
+    path,
   });
   res.clearCookie(process.env.JWT_REFRESH_TOKEN_COOKIE_NAME, {
     sameSite,
     secure,
+    path,
   });
 };
